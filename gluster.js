afdda30de49d54189b6453ac48d9972950952b8c
shindh <w7t@naver.com>
2016-12-21 08:54:44 +0000

    change port to 5434

--- gluster.js
+++ gluster.js
@@ -5,14 +5,50 @@ var ip = require('ip');
 var webSocketServer = require('websocket').server;
 var webSocketClient = require('websocket').client;
 
-var server = http.createServer(function(request, response) {
-});
+var credentials = {};
+var container_name = 'node_container';
+if (process.env.VCAP_SERVICES) { 
+	// cloud env. �꽕�젙. �뜲�씠�꽣 援ъ“�뒗 2.3.4 VCAP_SERVICES �솚寃쎌젙蹂� 李멸퀬
+	var services = JSON.parse(process.env.VCAP_SERVICES); 
+	var glusterfsConfig = services["glusterfs"]; 
 
-server.listen(5433, function() {
+	if (glusterfsConfig) { 
+		var config = glusterfsConfig[0]; 
+		console.log('GlusterFS Services: ' + glusterfsConfig);
+		credentials = {
+			provider: 'openstack', // 
+		    username: config.credentials.username,
+		    password: config.credentials.password,
+			authUrl:  config.credentials.auth_url.substring(0, config.credentials.auth_url.lastIndexOf('/')),
+			region: 'RegionOne' //
+		};
+	}
+} else {
+	// local env.
+	credentials = {
+		provider: 'openstack',
+	    username: 'cf13d551d997458e', 
+	    password: 'b45cc01d53a4f0e0',
+		authUrl:  'http://54.199.136.22:5000/',
+		region: 'RegionOne'
+	};
+	console.log('local env: ' + credentials);
+}
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = normalizePort(process.env.PORT || '5434');
+
+var webSocketServer = require('websocket').server;
+
+var server = http.createServer(function(request, response) {
 	console.log('==============================================================');
 	console.log('======== This is sample node.js server for Gluster FS ========');
 	console.log('==============================================================');
 });
+
+server.listen(port);
 
 var wsServer = new webSocketServer({
 	httpServer : server
@@ -25,79 +61,29 @@ function SocketServerOperation(request) {
 	console.log('======== New Connection Connected ========');
 	console.log('==========================================');
 
-	var credentials = {};
-	var container_name = 'node_container';
-	if (process.env.VCAP_SERVICES) { 
-		// cloud env. 설정. 데이터 구조는 2.3.4 VCAP_SERVICES 환경정보 참고
-		var services = JSON.parse(process.env.VCAP_SERVICES); 
-		var glusterfsConfig = services["glusterfs"]; 
-
-		if (glusterfsConfig) { 
-			var config = glusterfsConfig[0]; 
-			console.log('GlusterFS Services: ' + glusterfsConfig);
-			credentials = {
-				provider: 'openstack', // 
-			    username: config.credentials.username,
-			    password: config.credentials.password,
-				authUrl:  config.credentials.auth_url.substring(0, config.credentials.auth_url.lastIndexOf('/')),
-				region: 'RegionOne' //
-			};
-		}
-	} else {
-		// local env.
-		credentials = {
-			provider: 'openstack',
-		    username: 'cf13d551d997458e', 
-		    password: 'b45cc01d53a4f0e0',
-			authUrl:  'http://54.199.136.22:5000/',
-			region: 'RegionOne'
-		};
-		console.log('local env: ' + credentials);
-	}
-
 	var connection = request.accept(null, request.origin);
-
+	
 	connection.on('message', function(msg) {
 
 	});
 }
 
 /**
- * Event listener for HTTP server "error" event.
+ * Normalize a port into a number, string, or false.
  */
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
+  if (port >= 0) {
+    // port number
+    return port;
   }
-}
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  return false;
 }
