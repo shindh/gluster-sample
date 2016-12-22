a426abf073329255f17dee256e4128933f7c3040
AzureAD\신동현 <w7t@naver.com>
2016-12-22 16:42:38 +0900

    add forever module

--- gluster.js
+++ gluster.js
@@ -7,21 +7,23 @@ var webSocketClient = require('websocket').client;
 var formidable = require('formidable');
 var fs = require('fs');
 
+/**
+ * Read gluster fs data from system env value
+ */
 var credentials = {};
 var container_name = 'node_container';
 if (process.env.VCAP_SERVICES) { 
-	// cloud env. �꽕�젙. �뜲�씠�꽣 援ъ“�뒗 2.3.4 VCAP_SERVICES �솚寃쎌젙蹂� 李멸퀬
 	var services = JSON.parse(process.env.VCAP_SERVICES); 
 	var glusterfsConfig = services["glusterfs"]; 
 
 	if (glusterfsConfig) { 
 		var config = glusterfsConfig[0]; 
 		credentials = {
-			provider: 'openstack', // 
+			provider: config.credentials.provider,
 		    username: config.credentials.username,
 		    password: config.credentials.password,
 			authUrl:  config.credentials.auth_url.substring(0, config.credentials.auth_url.lastIndexOf('/')),
-			region: 'RegionOne' //
+			region: 'RegionOne'
 		};
 		console.log('provider: ' + credentials.provider);
 		console.log('username: ' + credentials.username);
@@ -41,53 +43,48 @@ if (process.env.VCAP_SERVICES) {
 	console.log('local env: ' + credentials.username);
 }
 
-// create Client
+/**
+ * create Client & Container
+ */
 var client = pkgcloud.storage.createClient(credentials);
-function connectToGlusterFS(cb) {
-	// check container
-	client.getContainer(container_name, function(err, container){
-	    if (err)
-	    {
-	    	console.log('getContainer result ERR');
-			// if container not exist
-	        if (err.statusCode === 404)
-	        {
-	      		// create container
-	            client.createContainer({name:container_name}, function(create_err, create_container){
-	                if (create_err) console.log(err);
-	        		else
-	        		{  
-				        // if container created successfully, setting a readable member(X-Contaner-Read: .r:*)
-			        	// 컨테이너가 성공적으로 생성되었다면 컨테이너를 누구나 읽을 수 있게 설정한다.(X-Contaner-Read: .r:*)
-				        // There is a bug in the code(pkgcloud). so i used api call.
-				        // pkgcloud 모듈에서 metadata를 넣을 경우 prefix가 붙는 로직때문에 제대로 위의 값이 입력이 안되므로 api를 통해서 설정.
-				        var serviceUrl = url.parse(create_container.client._serviceUrl);
-				        var option = {
-				            host: serviceUrl.hostname,
-				            port: serviceUrl.port,
-				            path: serviceUrl.path+'/'+container_name,
-				            method: 'POST',
-				            headers: {
-				              'X-Auth-Token': create_container.client._identity.token.id,
-				              'X-Container-Read': '.r:*' // ACL form
-				            }
-				        };
-				        var req = http.request(option, function(res){
-				          });
-				          req.end();
-	        		}
+client.getContainer(container_name, function(err, container){
+    if (err)
+    {
+    	console.log('getContainer result ERR');
+		// if container not exist
+        if (err.statusCode === 404)
+        {
+      		// create container
+            client.createContainer({name:container_name}, function(create_err, create_container){
+                if (create_err) console.log(err);
+        		else
+        		{  
+			        var serviceUrl = url.parse(create_container.client._serviceUrl);
+			        var option = {
+			            host: serviceUrl.hostname,
+			            port: serviceUrl.port,
+			            path: serviceUrl.path + '/' + container_name,
+			            method: 'POST',
+			            headers: {
+			              'X-Auth-Token': create_container.client._identity.token.id,
+			              'X-Container-Read': '.r:*' // ACL form
+			            }
+			        };
+			        var req = http.request(option, function(res){
+			        	console.log('create container result: ' + res.toString());
+			        });
+			        req.end();
+        		}
 
-	            });
-	        }
-	        else    
-	        	console.log(err);
-	    }
-	    else{
-	    	cb();
-	    }
-	});
-	
-}
+            });
+        }
+        else    
+        	console.log(err);
+    }
+    else{
+    	console.log('Successfully create container');
+    }
+});
 
 function uploadImg(cb){
 	console.log('UploadImg called..');
